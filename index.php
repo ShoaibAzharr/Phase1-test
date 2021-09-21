47f0ded5ff4f0eb8bcd6cbf014a130f87fbd8fff
Shoaib Azhar <shoaib.wpbrigade@gmail.com>
2021-09-21 13:35:09 +0500

    mod|fix: Concise code & fix validation

--- index.php
+++ index.php
@@ -2,214 +2,350 @@
 	<head>
 		<link rel="icon" href="download.jpg" type="image/x-icon">
 		
-		<title>WP Brigade Phase 1 Test</title>
+		<title>
+			WP Brigade Phase 1 Test
+		</title>
 		
-		<link rel="stylesheet" href="\phase1-test\style.css">
+		<link rel="stylesheet" href="\Phase1-test\style.css">
 		
 	</head>
 
 <!--PHP script start -->
 <?php
 
-	//declaration of variables
-		$name         = '';
-		$nameerr      = '';
-		$email        = '';
-		$emailerr     = '';
-		$number       = '';
-		$numbererr    = '';
-		$lname        = '';
-		$lnameerr     = '';
-		$address      = '';
-		$addresserr   = '';
-		$password     = '';
-		$passworderr  = '';
-		$cpassword    = '';
-		$cpassworderr = '';
-		$gender       = '';
-		$gendererr    = '';
-		$termserr     = '';
+	//definition of valuvariables
+		$name         = NULL;
+		$email        = NULL;
+		$number       = NULL;
+		$lname        = NULL;
+		$address      = NULL;
+		$password     = NULL;
+		$cpassword    = NULL;
+		$gender       = NULL;
+
+	//definition of error variables
+		$nameerr      = NULL;
+		$emailerr     = NULL;
+		$numbererr    = NULL;
+		$lnameerr     = NULL;
+		$addresserr   = NULL;
+		$passworderr  = NULL;
+		$cpassworderr = NULL;
+		$gendererr    = NULL;
+		$termserr     = NULL;
+
+	//bool to check if error exist
+		$error		  = false;
 
 
 	//if the form is submitted 
-	if($_SERVER['REQUEST_METHOD']=="POST")
+	if( isset( $_POST['submit'] ) )
 	{
 		
-		//Sanitization of form data
-			$name      = filter_var( $_REQUEST['name'], FILTER_SANITIZE_STRING );
-			$lname     = filter_var( $_REQUEST['lname'], FILTER_SANITIZE_STRING );
-			$email	   = filter_var( $_REQUEST['email'], FILTER_SANITIZE_EMAIL );
-			$number    = filter_var( $_REQUEST['number'], FILTER_SANITIZE_NUMBER_INT );
-			$address   = filter_var( $_REQUEST['address'], FILTER_SANITIZE_STRING );
-			$password  = filter_var( $_REQUEST['password'], FILTER_SANITIZE_STRING );
-			$cpassword = filter_var( $_REQUEST['cpassword'], FILTER_SANITIZE_STRING );
-			
+		//Sanitization of user data
+			$name      = filter_var( $_POST['name'], FILTER_SANITIZE_STRING );
+			$lname     = filter_var( $_POST['lname'], FILTER_SANITIZE_STRING );
+			$email	   = filter_var( $_POST['email'], FILTER_SANITIZE_EMAIL );
+			$number    = filter_var( $_POST['number'], FILTER_SANITIZE_NUMBER_INT );
+			$address   = filter_var( $_POST['address'], FILTER_SANITIZE_STRING );
+			$password  = filter_var( $_POST['password'], FILTER_SANITIZE_STRING );
+			$cpassword = filter_var( $_POST['cpassword'], FILTER_SANITIZE_STRING );
+
+		if( isset( $_POST['gender'] ) )
+		{
+			$gender    = filter_var( $_POST['gender'], FILTER_SANITIZE_STRING );
+		}
 		
 		//validation of form data
 
 		//first name
 		if ( empty( $name ) ){
+
 			$nameerr= "Enter name first";
+			$error = true;
+
 		}
 		
-		elseif ( !preg_match( "/^[a-zA-Z-' ]*$/",$name ) ) {
+		elseif ( !preg_match( "/^[a-zA-Z-' ]*$/", $name ) ) {
+
 			$nameerr = "Only letters and white space allowed";
+			$error = true;
+
 		}
-		elseif ( strlen( $name ) > 20 || strlen( $name ) < 2){
+		elseif ( 20 < strlen( $name )  || 2 > strlen( $name ) ){
+		
 			$nameerr = "First name must be between 2 to 20";
+			$error = true;
+
 		}
 
 		//last name
 		if ( empty( $lname ) ){
+		
 			$lnameerr = "Enter last name";
+			$error = true;
+
 		}
 		elseif ( !preg_match( "/^[a-zA-Z-' ]*$/", $lname ) ) {
+			
 			$lnameerr = "Only letters and white space allowed in last name";
+			$error = true;
+
 		}
-		elseif ( strlen( $lname ) > 20 || strlen( $lname ) < 2 ){
+		elseif ( 20 < strlen( $lname )  || 2 > strlen( $lname ) ){
+		
 			$lnameerr = "Last name must be between 2 to 20";
+			$error = true;
+
 		}
 
+
 		//gender
-		if ( array_key_exists ( 'gender', $_REQUEST ) ){
-			$gender = $_POST['gender'];
+		
+		if( empty( $gender ) ){
+			
+			$gendererr = "Please Select Gender";
+			$error = true;
+		
 		}
-		elseif( empty( $gender ) ){
-			$genderr = "Please Select Gender";
+		elseif ( "Male" !== $gender && "Female" !== $gender  ){
+			
+			$gendererr = "Please Select a valid gender!";
+			$error = true;
+
 		}
+		
 		
 		
 		//email
-		if ( empty ( $email ) ){
+		if ( empty( $email ) ){
+			
 			$emailerr = "Please Enter The Email";
+			$error = true;
+			
 		}
-		elseif ( !filter_var ( $email, FILTER_VALIDATE_EMAIL ) ){
+		elseif ( !filter_var( $email, FILTER_VALIDATE_EMAIL ) ){
+		
 			$emailerr = "Email is not valid!";
+			$error = true;
+
 		}
-		elseif ( strlen ( $email ) > 20 || strlen ( $email ) < 2 ){
+		elseif ( 20 < strlen( $email ) || 2 > strlen( $email ) ){
+			
 			$emailerr = "Email must be between 2 to 20";
+			$error = true;
+
 		}
 		
 		
 		//number
 		if ( empty( $number ) ){
+		
 			$numbererr = "please enter the number!";
+			$error = true;
+
 		}
 		elseif ( !preg_match( "/^[0][0-9]/", $number) ){
+			
 			$numbererr = "Number is not valid";
+			$error = true;
+
 		}
-		elseif ( strlen( $number ) !== 7){
+		elseif ( 7 !== strlen( $number ) ){
+			
 			$numbererr = "Phone number must contains 7 numbers" ;
+			$error = true;
+
 		}
 		
+
 		//address
-		if ( empty ( $address ) ){
+		if ( empty( $address ) ){
+			
 			$addresserr = "please enter the address!" ;
+			$error = true;
+
 		}
-		elseif ( !is_string( $address ) ){
-			$addresserr = "Address is not valid" ;
-		}
-		elseif ( strlen( $address ) > 50 || strlen( $address ) < 10 ){
+		elseif ( 50 < strlen( $address ) || 10 > strlen( $address ) ){
+		
 			$addresserr = "Address must be between 10 to 50";
+			$error = true;
+
 		}
 
-		//password
+
+		//password & confirm password
 		if ( empty( $password ) ){
+			
 			$passworderr =  "please enter the password!";
+			$error = true;
+
 		}
-		elseif ( strlen( $password ) > 20 || strlen( $password ) < 6 ){
-			$passworderr = "Password must be between 6 to 20" ;
+		else{
+
+			if ( 20 < strlen( $password ) || 6 > strlen( $password ) ){
+				
+				$passworderr = "Password must be between 6 to 20" ;
+				$error = true;
+
+			}
+			elseif ( empty( $cpassword ) ){
+				
+				$cpassworderr = "please enter the confirm password!" ;
+				$error = true;
+
+			}
+			elseif ( $password !== $cpassword ){
+				
+				$cpassworderr = "Password not matched!" ;
+				$error = true;
+
+			}
+			
 		}
-		elseif ( !is_string( $password ) ){
-			$passworderr ="Password is not valid" ;
-		}
-		
-		
-		//confirm password
-		if ( $password !== $cpassword ){
-			$cpassworderr = "Password not matched!" ;
-		}
-		elseif ( empty( $cpassword) ){
-			$cpassworderr = "please enter the confirm password!" ;
-		}
-		
 		
 		//terms
-		if ( !isset( $_REQUEST['terms'] ) ){
+		if ( !isset( $_POST['terms'] ) ){
+			
 			$termserr = "please accept the terms & conditions!";
+			$error = true;
+			
 		}
 
+		//validation end
 
 		//if there is no error
-		if( empty( $nameerr )     &&
-			empty( $lnameerr )    &&
-			empty( $emailerr )    &&
-			empty( $gendererr )   &&
-			empty( $numbererr )   &&
-			empty( $addresserr )  &&
-			empty( $passworderr ) &&
-			empty( $cpassworderr )
-		){
+		if( !$error ){
 		
 			echo "<h1>Your form is submitted $name! </h1>";
+			unset( $_POST['submit']);
 		
+		}
+		else{
+
+			echo "<h1>Please Check All Input Fields </h1>";
+			unset( $_POST['submit']);
+			
 		}
 	}
 ?>
 <!--PHP script ended -->
 
 	<body>
-		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+		<form action="" method="POST">
 			<!--First Name -->
-			<label for="name" id="label">First Name</label>	<span>*</span>
+		
+			<label for="name" id="label">
+				First Name
+			</label>	
+			<span>
+				*
+			</span>
+			
 			<?php echo "<span>" .$nameerr. "</span>" ?>
 			<br>
 			<input type="text" id="text" name="name" value="<?php echo $name;?>">
 
 			<!--Last name -->
+			
 			<br>
-			<label for="lname" id="label">Last Name</label>	<span>*</span>
+			<label for="lname" id="label">
+				Last Name
+			</label>	
+			<span>
+				*
+			</span>
 			<?php echo "<span>" .$lnameerr. "</span>"?>
 			<br>
 			<input type="text" id="text" name="lname" value="<?php echo $lname;?>">
 
 			<!--Gender -->
+			
 			<br>
-			<label for="gender">Gender</label> <span>*</span>
+			<label for="gender">
+				Gender
+			</label> 
+			<span>
+				*
+			</span>
 			<?php echo "<span>" .$gendererr. "</span>"?>
 			<br>
-			<input type="radio" name="gender" value="Male"
-			<?php if($gender==='Male'){echo "checked";}?>>Male
-			<input type="radio" name="gender" value="Female"
-			<?php if($gender==='Female'){echo "checked";}?>>Female
+			<input type="radio" name="gender" id="male" value="Male"
+				<?php 
+					if( 'Male' === $gender ){
+
+						echo "checked";
+
+					}
+				?>
+			>
+			<label for="male">
+				Male
+			</label>
+			<input type="radio" name="gender" id="female" value="Female"
+				<?php 
+					if( 'Female' === $gender ){
+
+						echo "checked";
+						
+					}
+				?>
+			>
+			<label for="female">
+				Female
+			</label>
 			
 			
 			<!--Email -->
+		
 			<br>
-			<label for="email">Email</label> <span>*</span>
+			<label for="email">
+				Email
+			</label> 
+			<span>
+				*
+			</span>
 			<?php echo "<span>" .$emailerr. "</span>"?>
 			<br>
 			<input type="text" id="text" name="email" value="<?php echo $email;?>">
 			
 			<!--Number -->
+			
 			<br>
-			<label for="number">Ph No.</label><span>*</span>
+			<label for="number">
+				Ph No.
+			</label>
+			<span>
+				*
+			</span>
 			<?php echo "<span>" .$numbererr. "</span>"?>
 			<br>
 			<input type="text"id="text" name="number" value="<?php echo $number; ?>">
 			
 			
 			<!--Address -->
+			
 			<br>
-			<label for="address">Address</label><span>*</span>
+			<label for="address">
+				Address
+			</label>
+			<span>
+				*
+			</span>
 			<?php echo "<span>" .$addresserr. "</span>"?>
 			<br>
 			<input type="text"id="text" name="address" value="<?php echo $address; ?>">
 			
 			
 			<!--Password -->
+			
 			<br>
-			<label for="password">Password</label><span>*</span>
+			<label for="password">
+				Password
+			</label>
+			<span>
+				*
+			</span>
 			<?php echo "<span>" .$passworderr. "</span>"?>
 			
 			<br>
@@ -217,25 +353,38 @@
 			
 			
 			<!--Confirm Password -->
+			
 			<br>
-			<label for="cpassword">Confirm Password</label><span>*</span>
+			<label for="cpassword">
+				Confirm Password
+			</label>
+			<span>
+				*
+			</span>
 			<?php echo "<span>" .$cpassworderr. "</span>"?>
 			<br>
 			<input type="password"id="text" name="cpassword" >
 			<br>
 
 			<!-- Terms-->
+			
 			<br>
 			<input type="checkbox" id="terms" name="terms">
-			<label for="terms">I accept terms & conditions</label><span>*</span>
+			<label for="terms">
+				I accept terms & conditions
+			</label>
+			<span>
+				*
+			</span>
 			<?php echo "<span>" .$termserr. "</span>"?>
 			<br>
 			
 			<!--Submit Button -->
-			<button type="submit">Submit
-			</button>
-
 			
+			<button type="submit" name=submit>
+				Submit
+			</button>
+	
 		</form>
 	</body>
 </html>
